fe30c3e546312c311bfd5e4afb7493cea48e6aca
anandhu-as <anandhuas010203@gmail.com>
2026-07-08 15:11:35 +0530

    added protected routes

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,10 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isProtectedRoute = createRouteMatcher(['/'])
+const isProtectedRoute = createRouteMatcher([
+  '/', 
+  '/coin(.*)',      
+  '/watchlist(.*)'
+])
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) {
