3946900461bda98b8c72f1fdb549405d0ff24a55
anandhu-as <anandhuas010203@gmail.com>
2026-07-10 09:57:49 +0530

    fixed watchlist/route

--- app/stores/useCoinStore.ts
+++ app/stores/useCoinStore.ts
@@ -1,23 +1,10 @@
 import { create } from "zustand";
-
 import { fetchCoinMarkets } from "@/lib/api/coins";
-import { CoinMarketsResponse } from "@/types/types";
-
-
-
-type CoinStore = {
-  coins: CoinMarketsResponse;
-  status: "idle" | "loading" | "success" | "error";
-  error: string | null;
-  fetchCoins: () => Promise<void>;
-};
-
+import { CoinStore } from "@/types/types";
 export const useCoinStore = create<CoinStore>((set) => ({
-
   coins: [],
   status: "idle",
   error: null,
-
 
   fetchCoins: async () => {
     set({ status: "loading", error: null });
